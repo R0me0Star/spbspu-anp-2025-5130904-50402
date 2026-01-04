b49348c8368591f1ef0b4fe6fb335f01c789e159
R0me0Star <eip_spb@mail.ru>
2026-01-04 22:25:23 +0300

    class Shape

--- pozdnyakov.egor/P5/main.cpp
+++ pozdnyakov.egor/P5/main.cpp
@@ -10,3 +10,17 @@ struct rectangle_t {
 	double height;
 	point_t pos;
 };
+
+class Shape {
+public: 
+	virtual double getArea() const = 0;
+	virtual rectangle_t getFrameRect() const = 0;
+
+	virtual void move(const point_t& pos) = 0;
+
+	virtual void move(double dx, double dy) = 0;
+
+	virtual void scale(double k) = 0;
+
+	virtual ~Shape() = default;
+};
